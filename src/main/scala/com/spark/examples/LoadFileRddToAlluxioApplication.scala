5456875f687a1cf5571ce0dc0332bad20fd72691
mazhiyong <wingmazhiyong@gmail.com>
2019-06-24 17:14:05 +0800

    rename

---
+++ src/main/scala/com/spark/examples/LoadFileRddToAlluxioApplication.scala
@@ -0,0 +1,24 @@
+package com.spark.examples
+
+import org.apache.spark.sql.SparkSession
+/**
+  * THe LoadFile's rdd to Alluxio
+  * Created by mazhiyong on 19/6/24.
+  * LoadFileRddToAlluxioApplication
+  *   -> gameplayRddAlluxio (game_id, user_id, game_type, channel_from, game_exp)
+  *     -> GameUserSummaryRddAlluxioPath(game_id, user_id, play_exp, play_count)
+  *       -> gameSummaryRddPath(game_id, uv, play_exp, play_count)
+  *       -> gameTypeSummaryRddPath(game_type, uv, play_exp, play_count)
+  */
+object LoadFileRddToAlluxioApplication {
+  def main(args: Array[String]): Unit = {
+    val gamePlayInputFile = args(0)
+    val gamePlayRddPath= args(1)
+    val spark = SparkSession.builder.appName("Simple Application RDD To Alluxio").getOrCreate()
+    val dataFrame = spark.read.json(gamePlayInputFile)
+    dataFrame.createTempView("game_play")
+    val gamePlayDF = spark.sql("select game_id, user_id, game_type, channel_from, game_exp from game_play")
+    gamePlayDF.rdd.saveAsTextFile(gamePlayRddPath)
+    spark.stop()
+  }
+}
