2bfe330ec26febf4848daf1f884d77ec8b85f636
mazhiyong <wingmazhiyong@gmail.com>
2019-07-05 10:39:38 +0800

    add application

--- src/main/scala/com/spark/examples/GameUserSummaryApplication.scala
+++ src/main/scala/com/spark/examples/GameUserSummaryApplication.scala
@@ -8,10 +8,10 @@ import org.apache.spark.sql.SparkSession
   */
 object GameUserSummaryApplication {
   def main(args: Array[String]): Unit = {
-    val gameplayRddAlluxioPath = args(0)
+    val gamePlayRddAlluxioPath = args(0)
     val gameUserSummaryRddAlluxioPath = args(1)
     val spark = SparkSession.builder.appName("Simple Application Read RDD From Alluxio").getOrCreate()
-    val dataFrame = spark.read.load(gameplayRddAlluxioPath)
+    val dataFrame = spark.read.load(gamePlayRddAlluxioPath)
     dataFrame.createTempView("game_play")
     val userGameSummaryDF = spark.sql("select game_id, user_id, sum(game_exp) play_exp, count(1) play_exp from game_play group by game_id, user_id")
     userGameSummaryDF.rdd.saveAsTextFile(gameUserSummaryRddAlluxioPath)
